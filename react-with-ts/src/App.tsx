a0dbb5dd29f262eae10b83cb49dbe2edbbed4d6d
marlonchiu <Knight1898@outlook.com>
2021-02-17 22:37:34 +0800

    3-11自定义hook第三部分-正确的方式完成URLLoader

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -4,7 +4,8 @@ import Hello from './components/Hello'
 import LikeButton from './components/LikeButton'
 import MouseTracker from './components/MouseTracker'
 // import useMousePosition from './hooks/useMousePosition'
-import withLoader from './components/withLoader'
+// import withLoader from './components/withLoader'
+import useURLLoader from './hooks/useURLLoader'
 import './App.css'
 
 interface ImageShowResult {
@@ -12,19 +13,22 @@ interface ImageShowResult {
   status: string;
 }
 
-const DogShow: React.FC<{ data: ImageShowResult }> = ({ data }) => {
-  return (
-    <>
-      <h2>Dog Show: {data.status}</h2>
-      <img src={data.message} alt=""/>
-    </>
-  )
-}
+// const DogShow: React.FC<{ data: ImageShowResult }> = ({ data }) => {
+//   return (
+//     <>
+//       <h2>Dog Show: {data.status}</h2>
+//       <img src={data.message} alt=""/>
+//     </>
+//   )
+// }
 
 function App() {
   const [ show, setShow ] = useState(true)
   // const positions = useMousePosition()
-  const WrapperDogShow = withLoader(DogShow, 'https://dog.ceo/api/breeds/image/random')
+  // const WrapperDogShow = withLoader(DogShow, 'https://dog.ceo/api/breeds/image/random')
+
+  const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random', [show])
+  const dogResult = data as ImageShowResult
 
   return (
     <div className="App">
@@ -34,7 +38,11 @@ function App() {
         <p>
           <button onClick={() => {setShow(!show)}}>toggle show</button>
         </p>
-        <WrapperDogShow />
+        {
+          loading ? <p>狗狗数据读取中</p>
+            : <img src={dogResult?.message} alt=""/>
+        }
+        {/* <WrapperDogShow /> */}
         <Hello message="Hello World!" />
         <LikeButton />
         { show && <MouseTracker /> }
---
+++ react-with-ts/src/hooks/useURLLoader.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useURLLoader = (url: string, deps: any[] = []) => {
+  const [data, setData] = useState<any>(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(url).then(result => {
+      setData(result.data)
+      setLoading(false)
+    })
+  }, deps)
+
+  return [data, loading]
+}
+
+export default useURLLoader
