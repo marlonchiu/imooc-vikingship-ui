5204dde01707e802ac3470eacc402e8a454c60eb
marlonchiu <Knight1898@outlook.com>
2021-02-21 14:30:25 +0800

    4-9 添加 Button 基本样式

--- vikingship-ui/src/App.tsx
+++ vikingship-ui/src/App.tsx
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Button>hello</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+        <Button disabled>Disabled Button</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
         <Button btnType={ButtonType.Link} href="https://www.baidu.com">BaiDu Link</Button>
-        <Button disabled>不可点击</Button>
+        <Button disabled btnType={ButtonType.Link} href="https://www.baidu.com">Disabled Link</Button>
       </header>
     </div>
   )
