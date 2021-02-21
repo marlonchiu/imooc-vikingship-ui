a4de4091b78ba622ebe96b12ff676923840407c4
marlonchiu <Knight1898@outlook.com>
2021-02-21 15:28:09 +0800

    5-2 通用测试框架 Jest 出场

---
+++ vikingship-ui/jest.test.js
@@ -0,0 +1,18 @@
+test('test common matcher', () => {
+  expect(2 + 2).toBe(4)
+  expect(2 + 2).not.toBe(5)
+})
+
+test('test to be true or false', () => {
+  expect(1).toBeTruthy()
+  expect(0).toBeFalsy()
+})
+
+test('test number', () => {
+  expect(4).toBeGreaterThan(3)
+  expect(2).toBeLessThan(3)
+})
+
+test('test object', () => {
+  expect({ name: 'viking' }).toEqual({ name: 'viking' })
+})
