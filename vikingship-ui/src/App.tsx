fd488d4783abea33e8877593da93235e47805fd4
marlonchiu <Knight1898@outlook.com>
2021-02-21 10:48:58 +0800

    4-8 小试牛刀 - Button 组件编码 第一部分

--- vikingship-ui/src/App.tsx
+++ vikingship-ui/src/App.tsx
@@ -1,31 +1,17 @@
-import React from 'react';
+import React from 'react'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
-      <h1>标题1</h1>
-      <h2>标题2</h2>
-      <h3>标题3</h3>
-      <h4>标题4</h4>
-      <hr />
-      <code>
-        const a = b;
-      </code>
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com">BaiDu Link</Button>
+        <Button disabled>不可点击</Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ vikingship-ui/src/components/Button/button.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export enum ButtonSize {
+  Large = 'large',
+  Small = 'small'
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link',
+}
+
+interface BaseButtonProps {
+  className?: string;
+  /**设置 Button 的禁用 */
+  disabled?: boolean;
+  /**设置 Button 的尺寸 */
+  size?: ButtonSize;
+  /**设置 Button 的类型 */
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+
+const Button: React.FC<BaseButtonProps> = (props) => {
+  const { btnType, disabled, size, children, className, href } = props
+  // btn, btn-lg, btn-primary
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === ButtonType.Link) && disabled
+  })
+
+  if (btnType === ButtonType.Link && href) {
+    return (
+      <a
+        className={classes}
+        href={href}
+      >
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <button
+        className={classes}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    )
+  }
+}
+
+Button.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
+}
+
+export default Button
