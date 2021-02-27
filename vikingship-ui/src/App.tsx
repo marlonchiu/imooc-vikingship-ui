9221526597a622797753bcc940aa38fb42a4dda3
marlonchiu <Knight1898@outlook.com>
2021-02-27 12:18:24 +0800

    7-1 图标解决方案简介

--- vikingship-ui/src/App.tsx
+++ vikingship-ui/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import ButtonDemo from './demos/button-demo'
 import AlertDemo from './demos/alert-demo'
 import MenuDemo from './demos/menu-demo'
@@ -7,6 +9,7 @@ import TabsDemo from './demos/tabs-demo'
 function App() {
   return (
     <div className="App">
+      <FontAwesomeIcon icon={faCoffee} size="10x"/>
       <TabsDemo />
       <MenuDemo />
       {/* <ButtonDemo /> */}
