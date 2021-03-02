e0ee5a8a7aa039bde0d71fcb21b6721d00134397
marlonchiu <Knight1898@outlook.com>
2021-03-02 23:53:15 +0800

    9-3 持续优化 - Input组件代码实现和优化过程

---
+++ vikingship-ui/src/components/Input/input.stories.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { Input } from './input'
+
+// 受控组件
+const ControlledInput = () => {
+  const [value, setValue] = useState()
+  return <Input value={value} defaultValue={value} onChange={(e) => { setValue(e.target.value) }} />
+}
+
+const defaultInput = () => (
+  <>
+    <Input
+      style={{width: '300px'}}
+      placeholder="placeholder"
+      onChange={action('changed')}
+    />
+    <ControlledInput />
+  </>
+)
+const inputWithDisabled = () => (
+  <Input
+    style={{width: '300px'}}
+    placeholder="disabled input"
+    disabled
+  />
+)
+
+const inputWithIcon = () => (
+  <Input
+    style={{width: '300px'}}
+    placeholder="input with icon"
+    icon="search"
+  />
+)
+
+const inputWithSize = () => (
+  <>
+    <Input
+      style={{width: '300px'}}
+      defaultValue="large size"
+      size="lg"
+    />
+    <Input
+      style={{width: '300px'}}
+      placeholder="small size"
+      size="sm"
+    />
+  </>
+)
+
+const inputWithPad = () => (
+  <>
+    <Input
+      style={{width: '300px'}}
+      defaultValue="prepend text"
+      prepend="https://"
+    />
+    <Input
+      style={{width: '300px'}}
+      defaultValue="google"
+      append=".com"
+    />
+  </>
+)
+
+
+storiesOf('Input component', module)
+  .add('Input', defaultInput)
+  .add('被禁用的 Input', inputWithDisabled)
+  .add('带图标的 Input', inputWithIcon)
+  .add('大小不同的 Input', inputWithSize)
+  .add('带前后缀的 Input', inputWithPad)
--- vikingship-ui/src/components/Input/input.tsx
+++ vikingship-ui/src/components/Input/input.tsx
@@ -1,14 +1,15 @@
 import React, { FC, ReactElement, InputHTMLAttributes, ChangeEvent } from 'react'
 import classNames from 'classnames'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
-import Icon from '../Icon'
+import Icon from '../Icon/icon'
+import { faDivide } from '@fortawesome/free-solid-svg-icons'
 
 export type InputSize = 'lg' | 'sm'
 export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 
 // Omit 忽略接口中的一直值
-interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, 'size'> {
-  /**是否禁用*/
+export interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, 'size'> {
+  /**是否禁用 Input*/
   disabled?: boolean;
   /**设置 input 大小，支持 lg 或者是 sm */
   size?: InputSize;
@@ -34,7 +35,6 @@ export const Input: FC<InputProps> = (props) => {
   const { disabled, size, icon, prepend, append, style, ...restProps } = props
   // 根据不同的属性计算className
 
-  // btn, btn-lg, btn-primary
   const classes = classNames('viking-input-wrapper', {
     [`input-size-${size}`]: size,
     'is-disabled': disabled,
@@ -43,14 +43,35 @@ export const Input: FC<InputProps> = (props) => {
     'input-group-prepend': !!prepend
   })
 
+  const fixControlledValue = (value: any) => {
+    if (typeof value === 'undefined' || value === null) {
+      return ''
+    }
+    return value
+  }
+
+  if ('value' in props) {
+    delete restProps.defaultValue
+    restProps.value = fixControlledValue(props.value)
+  }
+
   return (
     // 根据属性判断是否要添加不同的节点
-    null
+    <div className={classes} style={style}>
+      {prepend && <div className="viking-input-group-prepend">{prepend}</div>}
+      {icon && <div className="icon-wrapper"><Icon icon={icon} title={`title-${icon}`} /></div>}
+      <input
+        className="viking-input-inner"
+        disabled={disabled}
+        {...restProps}
+      />
+      {append && <div className="viking-input-group-append">{append}</div>}
+    </div>
   )
 }
 
 Input.defaultProps = {
-  disabled: false,
+  disabled: false
 }
 
 export default Input;
