46d26aaf33a74e3e3c6fed9f759a93dd3a60c899
marlonchiu <Knight1898@outlook.com>
2021-03-03 00:00:44 +0800

    修复 menu.test.tsx 报错

--- vikingship-ui/src/components/Menu/menu.test.tsx
+++ vikingship-ui/src/components/Menu/menu.test.tsx
@@ -3,6 +3,18 @@ import { render, RenderResult, fireEvent, cleanup, waitFor } from '@testing-libr
 import Menu, { MenuProps } from './menu'
 import MenuItem from './menuItem'
 import SubMenu from './subMenu'
+jest.mock('../Icon/icon', () => {
+  return () => {
+    return <i className="fa" />
+  }
+})
+jest.mock('react-transition-group', () => {
+  return {
+    CSSTransition: (props: any) => {
+      return props.children
+    }
+  }
+})
 
 const testProps: MenuProps = {
   defaultIndex: '0',
