69285352213ae8d8127fc086e2bc5b5195a78c70
marlonchiu <Knight1898@outlook.com>
2021-03-06 23:42:43 +0800

    9-7 AutoComplete 支持异步请求编码

--- vikingship-ui/src/components/AutoComplete/autoComplete.stories.tsx
+++ vikingship-ui/src/components/AutoComplete/autoComplete.stories.tsx
@@ -7,43 +7,59 @@ interface LakerPlayerProps {
   value: string;
   number: number;
 }
+interface GithubUserProps {
+  login: string;
+  url: string;
+  avatar_url: string;
+}
 const SimpleComplete = () => {
-  const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins',
-  'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
-  const lakersWithNumber = [
-    {value: 'bradley', number: 11},
-    {value: 'pope', number: 1},
-    {value: 'caruso', number: 4},
-    {value: 'cook', number: 2},
-    {value: 'cousins', number: 15},
-    {value: 'james', number: 23},
-    {value: 'AD', number: 3},
-    {value: 'green', number: 14},
-    {value: 'howard', number: 39},
-    {value: 'kuzma', number: 0},
-  ]
+  // const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins',
+  // 'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
+  // const lakersWithNumber = [
+  //   {value: 'bradley', number: 11},
+  //   {value: 'pope', number: 1},
+  //   {value: 'caruso', number: 4},
+  //   {value: 'cook', number: 2},
+  //   {value: 'cousins', number: 15},
+  //   {value: 'james', number: 23},
+  //   {value: 'AD', number: 3},
+  //   {value: 'green', number: 14},
+  //   {value: 'howard', number: 39},
+  //   {value: 'kuzma', number: 0},
+  // ]
 
-  // const renderOption = (item: DataSourceType<LakerPlayerProps>) => {
-  //   return (
-  //     <>
-  //       <h2>Name: {item.value}</h2>
-  //       <p>number: {item.number}</p>
-  //     </>
-  //   )
+  // const handleFetch = (query: string) => {
+  //   return lakers.filter(name => name.includes(query)).map(name => ({value: name}))
   // }
 
-
-  const handleFetch = (query: string) => {
-    return lakers.filter(name => name.includes(query)).map(name => ({value: name}))
-  }
   // const handleFetch = (query: string) => {
   //   return lakersWithNumber.filter(player => player.value.includes(query))
   // }
+  const handleFetch = (query: string) => {
+    return fetch(`https://api.github.com/search/users?q=${query}`)
+      .then(res => res.json())
+      .then(({ items }) => {
+        console.log(items)
+        const formatItems =  items.slice(0, 10).map((item: any) => ({ value: item.login, ...item}))
+        return formatItems
+      })
+  }
+
+  const renderOption = (item: DataSourceType) => {
+    const itemWithGithub = item as DataSourceType<GithubUserProps>
+    return (
+      <>
+        <h2>Name: {itemWithGithub.value}</h2>
+        <p>url: {itemWithGithub.url}</p>
+        <p>avatar_url: {itemWithGithub.avatar_url}</p>
+      </>
+    )
+  }
   return (
     <AutoComplete
       fetchSuggestions={handleFetch}
       onSelect={action('selected')}
-      // renderOption={renderOption}
+      renderOption={renderOption}
     />
   )
 }
--- vikingship-ui/src/components/AutoComplete/autoComplete.tsx
+++ vikingship-ui/src/components/AutoComplete/autoComplete.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useState, ChangeEvent, ReactElement } from 'react'
 import Input, { InputProps } from '../Input/input'
+import Icon from '../Icon/icon'
 
 interface DataSourceObject {
   value: string;
 }
 export type DataSourceType<T = {}> = T & DataSourceObject
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (str: string) => DataSourceType[];
+  fetchSuggestions: (str: string) => DataSourceType[] | Promise<DataSourceType[]>;
   onSelect?: (item: DataSourceType) => void;
   renderOption?: (item: DataSourceType) => ReactElement;
 }
@@ -22,14 +23,24 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   } = props
 
   const [inputValue, setInputValue] = useState(value as string)
-  const [ suggestions, setSuggestions ] = useState<DataSourceType[]>([])
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
   console.log('suggestions: ', suggestions)
+  const [ loading, setLoading ] = useState(false)
   const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim()
     setInputValue(value)
     if (value) {
       const results = fetchSuggestions(value)
-      setSuggestions(results)
+      // 判断是不是异步的获取数据
+      if (results instanceof Promise) {
+        setLoading(true)
+        results.then(data => {
+          setLoading(false)
+          setSuggestions(data)
+        })
+      } else {
+        setSuggestions(results)
+      }
     } else {
       setSuggestions([])
     }
@@ -70,6 +81,11 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
         onChange={handleChange}
         {...restProps}
       />
+      { loading &&
+        <ul className="suggestions-loading-icon">
+          <Icon icon="spinner" spin />
+        </ul>
+      }
       {(suggestions.length > 0) && generateDropdown()}
     </div>
   )
