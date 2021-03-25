ff71a0d679b136775589f269d62655f98ed2a5e1
marlonchiu <Knight1898@outlook.com>
2021-03-25 17:39:37 +0800

    update 更新

--- vikingship-ui/src/App.tsx
+++ vikingship-ui/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import ButtonDemo from './demos/button-demo'
 import AlertDemo from './demos/alert-demo'
 import MenuDemo from './demos/menu-demo'
@@ -8,8 +9,16 @@ import TransitionDemo from './demos/transition-demo'
 import InputDemo from './demos/input-demo'
 
 function App() {
+  const [title, setTitle] = useState('')
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1').then(response => {
+      console.log(response);
+      setTitle(response.data.title)
+    })
+  })
   return (
     <div className="App">
+      {title}
       <InputDemo />
       {/* <TransitionDemo /> */}
       {/* <IconDemo /> */}
