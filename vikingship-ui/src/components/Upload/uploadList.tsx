b784fa66e103cbfaba170d49c82fe6af64542442
marlonchiu <Knight1898@outlook.com>
2021-06-06 22:17:29 +0800

    10-9 显示上传进度 - 添加 Progress 组件

--- vikingship-ui/src/components/Upload/uploadList.tsx
+++ vikingship-ui/src/components/Upload/uploadList.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { UploadFile } from './upload'
 import Icon from '../Icon/icon'
+import Progress from '../Progress/progress'
 
 interface UploadListProps {
   fileList: UploadFile[];
@@ -30,12 +31,16 @@ export const UploadList: FC<UploadListProps> = (props) => {
             <span className="file-actions">
               <Icon icon="times" onClick={() => { onRemove(item)}}/>
             </span>
+            {item.status === 'uploading' &&
+              <Progress
+                percent={item.percent || 0}
+              />
+            }
           </li>
         )
       })}
     </ul>
   )
-
 }
 
 export default UploadList
