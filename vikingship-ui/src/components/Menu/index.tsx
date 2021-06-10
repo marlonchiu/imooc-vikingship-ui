91d5f6a19e5f3f22800a9e2e9b6aae93c63b95bb
marlonchiu <Knight1898@outlook.com>
2021-06-10 15:02:03 +0800

    11-4 创建组件库模块入口文件

--- vikingship-ui/src/components/Menu/index.tsx
+++ vikingship-ui/src/components/Menu/index.tsx
@@ -3,6 +3,7 @@ import Menu, { MenuProps } from './menu'
 import SubMenu, { SubMenuProps } from './subMenu'
 import MenuItem, { MenuItemProps } from './menuItem'
 
+// 交叉类型定义
 export type IMenuComponent = FC<MenuProps> & {
   Item: FC<MenuItemProps>,
   SubMenu: FC<SubMenuProps>
--- vikingship-ui/src/index.tsx
+++ vikingship-ui/src/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export { default as Alert } from './components/Alert'
+export { default as AutoComplete } from './components/AutoComplete'
+export { default as Button } from './components/Button'
+export { default as Icon } from './components/Icon'
+export { default as Input } from './components/Input'
+export { default as Menu } from './components/Menu'
+export { default as Progress } from './components/Progress'
+export { default as Tabs } from './components/Tabs'
+export { default as Transition } from './components/Transition'
+export { default as Upload } from './components/Upload'
